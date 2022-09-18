09c1fc0e13673c6f71f1dc0c0c18c7496f21d525
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-18 21:21:07 +0530

    leranreact

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export function App() {
     <div className="name">
       <h1>Counter Project </h1>
       <h1>{count}</h1>
-      <button onClick={() => setcount(count - 1)}>Decrement</button>
+      {/* <button onClick={() => setcount(count - 1)}>Decrement</button> */}
       <button onClick={() => setcount(count + 1)}>Increment</button>
       <button onClick={() => setcount(0)}>Reset</button>
     </div>

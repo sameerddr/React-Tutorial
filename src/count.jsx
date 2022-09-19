4515ec0180dea3bf59baf96b508169d42f0b4897
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-19 12:27:14 +0530

    count

---
+++ src/count.jsx
@@ -0,0 +1,27 @@
+// import React, { useState } from "react";
+// import "./App.css";
+// import User from "./users";
+// import Event from "./event";
+// import State from "./state";
+// import Props from "./props";
+// import logo from "./logo.svg";
+// import Profile from "./ifelse";
+// import Valid from "./valid";
+// export function App() {
+//     const [count, setcount] = useState(0);
+//     return ( <
+//         div className = "name" >
+//         <
+//         h1 > Counter Project By React < /h1> <
+//         h1 > { count } < /h1> <
+//         button onClick = {
+//             () => setcount(count - 1) } > Decrement < /button> <
+//         button onClick = {
+//             () => setcount(count + 1) } > Increment < /button> <
+//         button onClick = {
+//             () => setcount(0) } > Reset < /button> <
+//         /div>
+//     );
+// }
+
+// export default App;

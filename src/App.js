f4df69c410fa982f883c43daf10a14355ee6eb79
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-19 12:27:45 +0530

    count

--- src/App.js
+++ src/App.js
@@ -9,15 +9,7 @@ import Profile from "./ifelse";
 import Valid from "./valid";
 export function App() {
   const [count, setcount] = useState(0);
-  return (
-    <div className="name">
-      <h1>Counter Project By React</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setcount(count - 1)}>Decrement</button>
-      <button onClick={() => setcount(count + 1)}>Increment</button>
-      <button onClick={() => setcount(0)}>Reset</button>
-    </div>
-  );
+  return <div className="name"></div>;
 }
 
 export default App;

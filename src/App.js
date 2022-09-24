9931bb84bf6d1f6bb4e6e7839470d17cbbcad400
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-24 11:01:33 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import "./App.css";
 // import Form from "./Form";
 // import User from "./users";
 // import Event from "./event";
-
+// import State from "./state";
+// import Props from "./props";
+// import logo from "./logo.svg";
+// import Profile from "./ifelse";
+// import Valid from "./valid";
+// import Insta from "./Instafeed";
 // import FunProps from "./FunProps";
 // import Constrructor from "./lifecycle/Constrructor";
 // import Reuse from "./ReuseComponent/Reuse";

78e6167e60d02ae2a912dcccfc868b5d048a6779
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-27 22:16:43 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -20,11 +20,7 @@ import "./App.css";
 // import Pure from "./purecomponent/Pure";
 
 function App() {
-  return (
-    <div className="name">
-      <h1>gits </h1>
-    </div>
-  );
+  return <div className="name"></div>;
 }
 
 export default App;

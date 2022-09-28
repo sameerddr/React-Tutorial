938ed6fb60de639aa94b2d86e5a1b0c979175e5e
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-28 10:37:35 +0530

    Useref

--- src/App.js
+++ src/App.js
@@ -20,9 +20,21 @@ import "./App.css";
 // import Pure from "./purecomponent/Pure";
 
 function App() {
-    const inputRef = useRef(null);
-  return <div className="name">
-    </div>;
+  const inputRef = useRef(null);
+
+  function controlhandle() {
+    inputRef.current.style.color = "red";
+    inputRef.current.style.backgroundColor = "blue";
+    inputRef.current.focus();
+  }
+
+  return (
+    <div className="name">
+      <h1>UseRef</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={controlhandle}>Handle Input</button>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/UseRef.jsx
+++ src/hooks/UseRef.jsx
@@ -1,4 +1,23 @@
-// import React, { useRef } from "react";
-// function UseRef() {
-    
+// import React from "react";
+// import { useEffect, useState, useRef } from "react";
+// import "./App.css";
+
+// function App() {
+//   const inputRef = useRef(null);
+
+//   function controlhandle() {
+//     inputRef.current.style.color = "red";
+//     inputRef.current.style.backgroundColor = "blue";
+//     inputRef.current.focus();
+//   }
+
+//   return (
+//     <div className="name">
+//       <h1>UseRef</h1>
+//       <input type="text" ref={inputRef} />
+//       <button onClick={controlhandle}>Handle Input</button>
+//     </div>
+//   );
 // }
+
+// export default App;
